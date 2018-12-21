8c507c3ecc08c84a252995cc95ef0d9c879b346b
Steven G. Harms <steven.harms@flatironschool.com>
2018-12-21 08:38:30 -0500

    Whitespace cleanup

--- spec/rake_spec.rb
+++ spec/rake_spec.rb
@@ -2,24 +2,20 @@ require 'spec_helper'
 require 'rake'
 
 
-describe "Rakefile" do 
-  before(:all) do 
+describe "Rakefile" do
+  before(:all) do
     load File.expand_path("../../Rakefile", __FILE__)
   end
 
   describe 'namespace :greeting' do
     describe 'greeting:hello' do
-
       it "should print out 'hello from Rake!'" do
         expect($stdout).to receive(:puts).with("hello from Rake!")
         Rake::Task["greeting:hello"].invoke
       end
-
-
     end
 
     describe 'greeting:hola' do
-
       it "should print out 'hola de Rake!'" do
         expect($stdout).to receive(:puts).with("hola de Rake!")
         Rake::Task["greeting:hola"].invoke
@@ -28,8 +24,7 @@ describe "Rakefile" do
   end
 
   describe 'namespace :db' do
-
-    describe 'db:migrate' do 
+    describe 'db:migrate' do
       it "invokes the :environment task as a dependency" do
         expect(Rake::Task["db:migrate"].prerequisites).to include("environment")
       end
@@ -42,11 +37,10 @@ describe "Rakefile" do
     end
 
     describe 'db:seed' do
-
       before(:each) do
         clear_database
         recreate_table
-      end 
+      end
 
       it "seeds the database with dummy data from a seed file" do
         Rake::Task["db:seed"].invoke
@@ -57,11 +51,6 @@ describe "Rakefile" do
         expect(dummy_data[1][1]).to eq("April")
         expect(dummy_data[4][2]).to eq("10th")
       end
-
-
     end
-
   end
-
-
 end
