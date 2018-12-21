f3ffef42ffac5a08cb7fcccf0254a8631e363067
Steven G. Harms <steven.harms@flatironschool.com>
2018-12-21 08:48:24 -0500

    Adds console task test

--- spec/rake_spec.rb
+++ spec/rake_spec.rb
@@ -23,6 +23,12 @@ describe "Rakefile" do
     end
   end
 
+  describe 'console' do
+    it 'exists' do
+      expect(Rake::Task['console']).to be_truthy, "Make sure you have a 'console' rake task"
+    end
+  end
+
   describe 'namespace :db' do
     describe 'db:migrate' do
       it "invokes the :environment task as a dependency" do
